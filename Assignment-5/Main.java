202e4c6f3f380c258c78e2aa7f8999044a69e5bc
Aminul Islam <68509378+Aminul264@users.noreply.github.com>
2023-11-07 22:04:06 +0600

    Update Main.java

--- Assignment-5/Main.java
+++ Assignment-5/Main.java
@@ -1,1 +1,19 @@
 
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        WareHouse warehouse1 = new WareHouse("Warehouse 1");
+        WareHouse warehouse2 = new WareHouse("Warehouse 2");
+
+        warehouse1.addStock(new Item("SKU1"), 10);
+        warehouse2.addStock(new Item("SKU2"), 5);
+
+        List<WareHouse> warehouses = List.of(warehouse1, warehouse2);
+        OrderFulfillment orderFulfillment = new OrderFulfillment(warehouses);
+
+        Order order = new Order(List.of(new Item("SKU1"), new Item("SKU2"), new Item("SKU3")));
+
+        orderFulfillment.fulfillOrder(order);
+    }
+}
